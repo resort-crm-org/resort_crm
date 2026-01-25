5c69d185de43cc921ab66cdf2f78d6926e437fb4
23suca03-dineshkarthick <23suca03@tcarts.in>
2026-01-25 18:45:09 +0530

    A Small Change

--- src/main/java/com/resort/crm/exception/BadRequestException.java
+++ src/main/java/com/resort/crm/exception/BadRequestException.java
@@ -2,6 +2,6 @@ package com.resort.crm.exception;
 
 public class BadRequestException extends RuntimeException {
     public BadRequestException(String message) {
-        super(message);
+         super(message);
     }
 }
